3cd99e55126d8059de0ffd655a677407bfdc7af4
SpaceArk& <spacearkchief@gmail.com>
2020-09-17 01:04:10 +0700

    Updates

---
+++ precache-manifest.a93d341209d35cb82cd11b6912c8fc25.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cb4b72942480d5860fae4c3cce836b",
+    "url": "/SunsetFarm/index.html"
+  },
+  {
+    "revision": "237cbeb14b6111fac376",
+    "url": "/SunsetFarm/static/css/2.36bc8797.chunk.css"
+  },
+  {
+    "revision": "463e7a5769c2edb053c4",
+    "url": "/SunsetFarm/static/css/main.4925e85d.chunk.css"
+  },
+  {
+    "revision": "237cbeb14b6111fac376",
+    "url": "/SunsetFarm/static/js/2.03f2313f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SunsetFarm/static/js/2.03f2313f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463e7a5769c2edb053c4",
+    "url": "/SunsetFarm/static/js/main.736f4c11.chunk.js"
+  },
+  {
+    "revision": "20ee0d0e9f9e0badbf6c",
+    "url": "/SunsetFarm/static/js/runtime-main.30c27d60.js"
+  },
+  {
+    "revision": "2792c207472ffb2e8b8b4af5271ccf86",
+    "url": "/SunsetFarm/static/media/bg1.2792c207.jpg"
+  },
+  {
+    "revision": "2167fed1580f162e68099f448d1db74d",
+    "url": "/SunsetFarm/static/media/cloud1.2167fed1.png"
+  },
+  {
+    "revision": "41d1e7792a2728b629cc6364de045279",
+    "url": "/SunsetFarm/static/media/cloud2.41d1e779.png"
+  },
+  {
+    "revision": "7c878e086b8ba8d11993fd2cc1754379",
+    "url": "/SunsetFarm/static/media/cloud3.7c878e08.png"
+  },
+  {
+    "revision": "ea046c5c1b3407aab18460846eafe93f",
+    "url": "/SunsetFarm/static/media/cloud4.ea046c5c.png"
+  },
+  {
+    "revision": "34ae4c87d746dcf1f2e9fcb5fe049180",
+    "url": "/SunsetFarm/static/media/cloud5.34ae4c87.png"
+  },
+  {
+    "revision": "24ec684edc428e18e53347127571684e",
+    "url": "/SunsetFarm/static/media/coffee.24ec684e.jpg"
+  },
+  {
+    "revision": "ba513232ff8a36a0281c01db51b8aeee",
+    "url": "/SunsetFarm/static/media/fb.ba513232.png"
+  },
+  {
+    "revision": "5d637313152eb1c3aafeee1ad020a7ce",
+    "url": "/SunsetFarm/static/media/horse.5d637313.jpeg"
+  },
+  {
+    "revision": "e6efffe77135eababfa501af8de5e27a",
+    "url": "/SunsetFarm/static/media/logo.e6efffe7.png"
+  },
+  {
+    "revision": "08ff3cf8146bfb53dba99af73223e71a",
+    "url": "/SunsetFarm/static/media/ptk.08ff3cf8.jpg"
+  },
+  {
+    "revision": "ece5bb0974554113333039eed7a1100a",
+    "url": "/SunsetFarm/static/media/wa.ece5bb09.png"
+  }
+]);
